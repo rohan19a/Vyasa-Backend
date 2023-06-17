e7750a12c2640ad8c838c749fae07744d1a29e3a
rohan19a <arohan19a2@gmail.com>
2023-06-16 21:36:47 -0700

    added data, work on python queries

---
+++ src/model.py
@@ -0,0 +1,27 @@
+import mindsdb_sdk
+from queries import make_query
+
+# connects to the default port (47334) on localhost 
+server = mindsdb_sdk.connect()
+
+# connects to the specified host and port
+server = mindsdb_sdk.connect('http://127.0.0.1:47334')
+
+
+#https://docs.mindsdb.com/sql/project
+#project = server.get_project()
+
+#make_query('CREATE DATABASE project_alpha;')
+
+make_query('SHOW FULL DATABASES;')
+
+
+#print(project.list_models())
+
+'''
+my_model = project.create_model (
+    name = 'my_model',
+    predict = 'target',
+    query = my_table
+)
+'''
---
+++ src/queries.py
@@ -0,0 +1,13 @@
+import requests 
+
+url = 'http://127.0.0.1:47334/api/sql/query'
+
+def make_query(query):
+    cookies = {'session': '273trgsehgrui3i2riurwehe'}
+    resp = requests.post(url, json={'query': query}, cookies=cookies)
+    if resp.status_code != 200:
+        raise Exception('Query failed')
+    return resp.json()
+
+print(make_query('SELECT * FROM users'))
+
