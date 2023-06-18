7bbc977a067aa5e5988e0d5631f51bddac883efc
rohan19a <arohan19a2@gmail.com>
2023-06-17 17:50:40 -0700

    work

--- src/backend.py
+++ src/backend.py
@@ -1,49 +1,49 @@
 #backend for the website.
 
 from flask import Flask
-from queries import make_query
-from functions import decide, initialize_email
+from queries import make_query, query_postgres
+from functions import decide, initialize_email 
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/update', methods=['POST'])
 def update():
-    make_query('INSERT INTO EmailAddresses (email_id, user_id, email_address) VALUES (1, "test", "test")')
-    make_query('ISERT INTO EmailAttributes (attribute_id, email_id, name, role, description, authority, department), VALUES (1, 1, "test", "test", "test", "test", "test")')
+    query_postgres('INSERT INTO EmailAddresses (email_id, user_id, email_address) VALUES (1, "test", "test")')
+    query_postgres('ISERT INTO EmailAttributes (attribute_id, email_id, name, role, description, authority, department), VALUES (1, 1, "test", "test", "test", "test", "test")')
     return 'OK'
 
 @app.route('/get', methods=['GET'])
 def get():
-    make_query('SELECT * FROM EmailAddresses where userid = "test"')
+    query_postgres('SELECT * FROM EmailAddresses where emailaddresses.user_id = "test"')
     return 'OK'
 
 @app.route('/delete', methods=['DELETE'])
 def delete():
-    make_query('DELETE FROM EmailAddresses WHERE email_id = "test"')
+    query_postgres('DELETE FROM EmailAddresses WHERE email_id = "test"')
     return 'OK'
 
 @app.route('/create', methods=['PUT'])
 def create():
-    make_query('INSERT INTO EmailAddresses (email_id, user_id, email_address) VALUES (1, "test", "test")')
-    make_query('')
+    query_postgres('INSERT INTO EmailAddresses (email_id, user_id, email_address) VALUES (1, "test", "test")')
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    if len(make_query('SELECT * FROM Users WHERE username = "test"')) > 0:
+    if len(query_postgres('SELECT * FROM Users WHERE username = "test"')) > 0:
         return 'Username already exists'
     
     #create the new database for the user, from functions.py
-    initialize_email()
+    #initialize_email()
 
-    make_query('INSERT INTO Users (user_id, username, password) VALUES (1, "test", "test")')
+    query_postgres('INSERT INTO Users (user_id, username, password) VALUES (1, "test", "test")')
     return 'OK'
 
 @app.route('/login', methods=['POST'])
 def login():
-    l = make_query('SELECT * FROM Users WHERE username = "test" AND password = "test"')
+    l = query_postgres('SELECT * FROM Users WHERE username = "test" AND password = "test"')
     if len(l) == 0:
         return 'Invalid username or password'
-    return 'OK'
+    return jsonify({'success': True, 'ok': True})
 
 
 
